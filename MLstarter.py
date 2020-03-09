b11cac480fbcd7e01bccb0d79ca492b683d6c2ff
kk265 <kk265.dev@gmail.com>
2020-03-10 04:23:44 +0530

    init

---
+++ MLstarter.py
@@ -0,0 +1,23 @@
+# FILE HANDLING
+import os
+import pathlib as pt
+
+# DATA WRANGLING and MATRIX MATH
+import pandas as pd
+import numpy as np
+
+# GRAPHING
+from matplotlib import pyplot as plt
+import seaborn as sns
+# import altair
+import streamlit as st
+
+st.write('{}'.format())
+
+# EDA
+
+# HMM
+
+if __name__=='__main__':
+    #st.run()
+    pass
